85e583fd23879a84026e1b19521e450eee4054c2
JHussain99 <Junaidhussain112@outlook.com>
2022-08-01 20:02:07 +0100

    HelloEveryone

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-import Question from './fetch';
-import Question2 from './NewFetch';
-
-
-
-
+import logo from "./logo.svg";
+import "./App.css";
+import Question2 from "./NewFetch";
 
 function App() {
   return (
     <div className="App-header">
-      
       <header className="App">
-        <img src= "https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y3V0ZSUyMGNhdHxlbnwwfHwwfHw%3D&w=1000&q=80"></img>
-      
-        <p>
-           Edit   <code>src/App.js</code> and save to reload.
-        </p>
+        <Question2></Question2>
+        {/* <img src="https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y3V0ZSUyMGNhdHxlbnwwfHwwfHw%3D&w=1000&q=80"></img> */}
+
+        <p>{/* Edit <code>src/App.js</code> and save to reload. */}</p>
         <a
-        
-      
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
@@ -29,9 +20,7 @@ function App() {
         </a>
       </header>
     </div>
-    
-)
+  );
+}
 
-  }
-
-export default App
+export default App;
--- src/fetch.js
+++ src/fetch.js
@@ -1,11 +1,7 @@
-
-
-
 // import React from "react";
-// import { Axios } from "axios";
+// import axios from "axios";
 
-
-// class Question extends React.Component {
+// export default class Player extends React.Component {
 //     constructor(props) {
 //         super(props);
 //         this.state = {
@@ -18,42 +14,42 @@
 //     }
 //     componentDidMount() {
 //         axios({
-//           url: "http://localhost:5080/Spotify",
-//           method: "GET"
-//         })
-//         .then((result) => {
-//           this.setState({
-//             albumImageUrl: result.albumImageUrl,
-//             artist: result.artist,
-//             isPlaying: result.isPlaying,
-//             trackUrl: result.trackUrl,
-//             title: result.title
-//           });
-//         });
+//                 url: "http://localhost:5080/Spotify",
+//                 method: "GET"
+//             })
+//             .then((result) => {
+//                 this.setState({
+//                     albumImageUrl: result.albumImageUrl,
+//                     artist: result.artist,
+//                     isPlaying: result.isPlaying,
+//                     trackUrl: result.trackUrl,
+//                     title: result.title
+//                 });
+//             });
 //     }
 //     render() {
 //         const { albumImageUrl } = this.state;
 //         const { artist } = this.state;
 //         const { isPlaying } = this.state;
-//         return(
-//             <div>
-//                 <div>
-//                     {albumImageUrl}
-//                 </div>
-//                 <div>
-//                     {artist}
-//                 </div>
-//                 <div>
-//                     {isPlaying}
-//                 </div>
-//                 <div>
-//                     {trackUrl}
-//                 </div>
-//                 <div>
-//                     {title}
-//                 </div>
-               
-//                 <button type="button" onClick={() => this.answer.setState(true)}>Reveal answer</button>
-//             </div>
+//         return ( <
+//             div >
+//             <
+//             div > { albumImageUrl } <
+//             /div> <
+//             div > { artist } <
+//             /div> <
+//             div > { isPlaying } <
+//             /div> <
+//             div > { trackUrl } <
+//             /div> <
+//             div > { title } <
+//             /div>
+
+//             <
+//             button type = "button"
+//             onClick = {
+//                 () => this.answer.setState(true) } > Reveal answer < /button> <
+//             /div>
 //         );
-//     }}export default Question;
+//     }
+// }
